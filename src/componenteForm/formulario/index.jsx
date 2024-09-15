e86b0533ec882c7d0ceb092c39fd5adb20333c0f
Junior Teixeira <teixeirajunior19051988@gmail.com>
2024-09-15 00:13:10 -0300

    Adição de título para o formulário

--- src/componenteForm/formulario/index.jsx
+++ src/componenteForm/formulario/index.jsx
@@ -1,7 +1,7 @@
 /*
  Componente Formulário onde faço a importação do componente FormInput do formulário css,
  nele faço o uso de três componentes FormInput onde o primeiro é definido o nome, 
- no segundo o email e no terceiro o input de enviar 
+ no segundo o email e no terceiro o input de enviar com o seu título.
 */
 
 import FormInput from '../formInput';
@@ -10,11 +10,14 @@ import './mobile.css';
 
 const Formulario = () => {
     return(
-        <form className='container-form'>
-            <FormInput inputName={"Nome"} type={"name"} required={"required"}  placeholder={"Seu nome completo"} />
-            <FormInput inputName={"Email"} type={"email"} required={"required"}  placeholder={"seuemail@email.com"} />
-            <FormInput type={"submit"}/>
-        </form>
+        <>
+          <h2>Gostou entre em contato</h2>
+          <form className='container-form'>
+              <FormInput inputName={"Nome"} type={"name"} required={"required"}  placeholder={"Seu nome completo"} />
+              <FormInput inputName={"Email"} type={"email"} required={"required"}  placeholder={"seuemail@email.com"} />
+              <FormInput type={"submit"}/>
+          </form>
+        </>
     );
 };
 
